c5829832e5d23c7111715f273c7881c099bc6bbc
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-21 23:00:40 +0400

    запрос на открытие файла

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ var open_file = document.querySelector('.open_file');
     var xmlhttp = new XMLHttpRequest();
     var file = 'D:/PythonProjects/python_tests/Stepan_tasks/string_encode.py';
     var line = '3';
-    xmlhttp.open("GET", "http://localhost:63342/api/file?file="
+    xmlhttp.open("GET", "http://localhost:63341/api/file?file="
         + file + "&line=" + line + "&column=3");
     xmlhttp.send();
 };
