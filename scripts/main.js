8a4fcced039fae886b2dd57d4e9ec44fb136f332
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-21 22:03:45 +0400

    запрос на открытие файла

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,11 @@
-var link = document.querySelector('.my_link');
-var img = document.querySelector('img');
-var for_user = document.querySelector('h2');
-var changeUserButton = document.querySelector('.change_user');
-var closeMeeting = document.querySelector('.close_meeting');
-link.onclick = function () {
-    alert('Сначала нажмите на ОК плииз');
-};
 
-// Кнопка "Закрыть приветствие"
-closeMeeting.onclick = function(){
-    if (localStorage.getItem('name') && for_user.innerHTML === ''){
-        localStorage.removeItem('name');
-        closeMeeting.title = 'Закрыть приветствие';
-    }
-  for_user.innerHTML = '';
-    closeMeeting.title = 'Удалить имя из localstorage';
-};
+var open_file = document.querySelector('.open_file');
 
-// метод смены изображение при книке на него
-img.onclick = function () {
-    var currentImg = img.getAttribute('src');
-    if (currentImg === 'images/cat.jpeg') {
-        img.setAttribute('src', 'images/little_cat.jpg');
-    } else {
-        img.setAttribute('src', 'images/cat.jpeg');
-    }
-};
-// добавление переменной name в localstorage
-function setUserName() {
-    var myName = prompt('Пожалуйста введите имя для приветствия');
-    if (myName !== null && myName !== '') {
-        localStorage.setItem('name', myName);
-        for_user.innerHTML = 'Приветствую, ' + myName;
-        closeMeeting.title = 'Закрыть приветствие'
-    }
-}
-if (localStorage.getItem('name') !== null){
-    for_user.innerHTML = 'Приветствую, ' + localStorage.getItem('name');
-    closeMeeting.title = 'Закрыть приветствие'
-}
-else{
-    localStorage.removeItem('name');
-    setUserName();
-}
-
-// Кнопка "Сменить пользователя"
-changeUserButton.onclick = function () {
-    setUserName();
+ function go_to_file(){
+    var xmlhttp = new XMLHttpRequest();
+    var file = 'D:/PythonProjects/python_tests/Stepan_tasks/string_encode.py';
+    var line = '3';
+    xmlhttp.open("GET", "http://localhost:63342/api/file?file="
+        + file + "&line=" + line + "&column=3");
+    xmlhttp.send();
 };
