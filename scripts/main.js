70d910ebe59365534028393e2c8b4ff743fe98f6
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-21 23:16:27 +0400

    запрос на открытие файла

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var open_file = document.querySelector('.open_file');
 
  function go_to_file(){
     var xmlhttp = new XMLHttpRequest();
-    var file = 'D:/PythonProjects/python_tests/Stepan_tasks/string_encode.py';
+    var file = 'C:/Api-tests/19.511/mobile/test-message-new/test_contacts_read.py';
     var line = '3';
     xmlhttp.open("GET", "http://localhost:63341/api/file?file="
         + file + "&line=" + line + "&column=3");
